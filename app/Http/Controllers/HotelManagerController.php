94a13030ba81a731165465ddef7bcd804e2faaed
chhumsothatt54 <chhumsothatt54@gmail.com>
2026-09-21 01:46:02 +0700

    push

--- app/Http/Controllers/HotelManagerController.php
+++ app/Http/Controllers/HotelManagerController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Amenity;
+use App\Models\User;
+use App\Models\UserNotification;
 use App\Models\Booking;
 use App\Models\Hotel;
 use App\Models\HotelImage;
